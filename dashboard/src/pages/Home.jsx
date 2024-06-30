1054b224ab646195ef3afe334846384c7d307c58
tamim-dev <tanvirmahmudtamim59@gmail.com>
2024-06-30 15:55:55 +0600

    design

--- dashboard/src/pages/Home.jsx
+++ dashboard/src/pages/Home.jsx
@@ -4,303 +4,287 @@ import { Button, Form, Input, Select, Space, message } from "antd";
 import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
 
 const optionsDepartment = [
-    { value: "BBA", label: "BBA" },
-    { value: "TMS", label: "TMS" },
-    { value: "THM", label: "THM" },
-    { value: "MBA", label: "MBA" },
-    { value: "BFA", label: "BFA" },
-    { value: "CSE/MCSE", label: "CSE / MCSE" },
-    { value: "BSED/MSED", label: "BSED / MSED" },
+  { value: "BBA", label: "BBA" },
+  { value: "TMS", label: "TMS" },
+  { value: "THM", label: "THM" },
+  { value: "MBA", label: "MBA" },
+  { value: "BFA", label: "BFA" },
+  { value: "CSE/MCSE", label: "CSE / MCSE" },
+  { value: "BSED/MSED", label: "BSED / MSED" },
 ];
 const optionsSemester = [
-    { value: "1st", label: "1st" },
-    { value: "2nd", label: "2nd" },
-    { value: "3rd", label: "3rd" },
-    { value: "4th", label: "4th" },
-    { value: "5th", label: "5th" },
-    { value: "6th", label: "6th" },
-    { value: "7th", label: "7th" },
-    { value: "8th", label: "8th" },
+  { value: "1st", label: "1st" },
+  { value: "2nd", label: "2nd" },
+  { value: "3rd", label: "3rd" },
+  { value: "4th", label: "4th" },
+  { value: "5th", label: "5th" },
+  { value: "6th", label: "6th" },
+  { value: "7th", label: "7th" },
+  { value: "8th", label: "8th" },
 ];
 const optionsSection = [
-    { value: "2016-2017", label: "2016-2017" },
-    { value: "2017-2018", label: "2017-2018" },
-    { value: "2018-2019", label: "2018-2019" },
-    { value: "2019-2020", label: "2019-2020" },
-    { value: "2020-2021", label: "2020-2021" },
-    { value: "2021-2022", label: "2021-2022" },
-    { value: "2022-2023", label: "2022-2023" },
-    { value: "2023-2024", label: "2023-2024" },
+  { value: "2016-2017", label: "2016-2017" },
+  { value: "2017-2018", label: "2017-2018" },
+  { value: "2018-2019", label: "2018-2019" },
+  { value: "2019-2020", label: "2019-2020" },
+  { value: "2020-2021", label: "2020-2021" },
+  { value: "2021-2022", label: "2021-2022" },
+  { value: "2022-2023", label: "2022-2023" },
+  { value: "2023-2024", label: "2023-2024" },
 ];
 const Home = () => {
-    const [messageApi, contextHolder] = message.useMessage();
+  const [messageApi, contextHolder] = message.useMessage();
 
-    const success = () => {
-        messageApi.open({
-            type: "success",
-            content: "Add student result",
-        });
+  const success = () => {
+    messageApi.open({
+      type: "success",
+      content: "Add student result",
+    });
+  };
+
+  const onFinish = async (values) => {
+    let data = {
+      department: values.department,
+      session: values.session,
+      semester: values.semester,
+      roll: values.roll,
+      name: values.name,
+      fathername: values.fathername,
+      mothername: values.mothername,
+      gpa: values.gpa,
+      subjects: values.subject,
     };
 
-    const onFinish = async (values) => {
-        let data = {
-            department: values.department,
-            session: values.session,
-            semester: values.semester,
-            roll: values.roll,
-            name: values.name,
-            fathername: values.fathername,
-            mothername: values.mothername,
-            gpa: values.gpa,
-            subjects: values.subject,
-        };
+    let user_data = await axios.post("http://localhost:8000/addresult", data);
 
-        let user_data = await axios.post(
-            "http://localhost:8000/addresult",
-            data
-        );
+    if (user_data.data.success) {
+      success();
+    }
+  };
 
-        if (user_data.data.success) {
-            success();
-        }
-    };
+  return (
+    <>
+      {contextHolder}
+      <Form
+        name="basic"
+        labelCol={{
+          span: 5,
+        }}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "50px",
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <h1 style={{ marginBottom: "20px" }}>Add student result</h1>
 
-    return (
-        <>
-            {contextHolder}
-            <Form
-                name="basic"
-                labelCol={{
-                    span: 5,
-                }}
-                style={{
+        <Form.Item
+          style={{ width: "600px" }}
+          label="Roll"
+          name="roll"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Role",
+            },
+          ]}
+        >
+          <Input type="number" />
+        </Form.Item>
+        <Form.Item
+          style={{ width: "600px" }}
+          label="Name"
+          name="name"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Email!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          style={{ width: "600px" }}
+          label="Father Name"
+          name="fathername"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Email!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          style={{ width: "600px" }}
+          label="Mother Name"
+          name="mothername"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Email!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          style={{ width: "600px" }}
+          label="GPA"
+          name="gpa"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Email!",
+            },
+          ]}
+        >
+          <Input type="number" />
+        </Form.Item>
+
+        <Form.Item
+          style={{ width: "600px" }}
+          name="department"
+          label="Department"
+          rules={[
+            {
+              required: true,
+              message: "Please select Department",
+            },
+          ]}
+        >
+          <Select
+            options={optionsDepartment}
+            placeholder="Select Department"
+          ></Select>
+        </Form.Item>
+        <Form.Item
+          style={{ width: "600px" }}
+          name="semester"
+          label="Semester"
+          rules={[
+            {
+              required: true,
+              message: "Please select Semester",
+            },
+          ]}
+        >
+          <Select
+            options={optionsSemester}
+            placeholder="Select Semester"
+          ></Select>
+        </Form.Item>
+        <Form.Item
+          style={{ width: "600px" }}
+          name="session"
+          label="Session"
+          rules={[
+            {
+              required: true,
+              message: "Select session",
+            },
+          ]}
+        >
+          <Select
+            options={optionsSection}
+            placeholder="Select session"
+          ></Select>
+        </Form.Item>
+
+        <Form.List name="subject">
+          {(fields, { add, remove }) => (
+            <>
+              {fields.map(({ key, name, ...restField }) => (
+                <Space
+                  key={key}
+                  style={{
                     display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginTop: "50px",
-                }}
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                autoComplete="off"
-            >
-                <h1>Add student result</h1>
+                    marginBottom: 8,
+                  }}
+                  align="baseline"
+                >
+                  <Form.Item
+                    {...restField}
+                    name={[name, "subjectname"]}
+                    rules={[
+                      {
+                        required: true,
+                        message: "Missing first name",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Subject Name" />
+                  </Form.Item>
+                  <Form.Item
+                    {...restField}
+                    name={[name, "subjectcode"]}
+                    rules={[
+                      {
+                        required: true,
+                        message: "Missing last name",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Subject Code " type="number" />
+                  </Form.Item>
+                  <Form.Item
+                    {...restField}
+                    name={[name, "score"]}
+                    rules={[
+                      {
+                        required: true,
+                        message: "Missing last name",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Score" type="number" />
+                  </Form.Item>
+                  <Form.Item
+                    {...restField}
+                    name={[name, "gpa"]}
+                    rules={[
+                      {
+                        required: true,
+                        message: "Missing last name",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="GPA " />
+                  </Form.Item>
+                  <MinusCircleOutlined onClick={() => remove(name)} />
+                </Space>
+              ))}
+              <Form.Item>
+                <Button
+                  type="dashed"
+                  onClick={() => add()}
+                  block
+                  icon={<PlusOutlined />}
+                >
+                  Add Subject
+                </Button>
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
 
-                <Form.Item
-                    style={{ width: "600px" }}
-                    label="Roll"
-                    name="roll"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your Role",
-                        },
-                    ]}
-                >
-                    <Input type="number" />
-                </Form.Item>
-                <Form.Item
-                    style={{ width: "600px" }}
-                    label="Name"
-                    name="name"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your Email!",
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    style={{ width: "600px" }}
-                    label="Father Name"
-                    name="fathername"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your Email!",
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    style={{ width: "600px" }}
-                    label="Mother Name"
-                    name="mothername"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your Email!",
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    style={{ width: "600px" }}
-                    label="GPA"
-                    name="gpa"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your Email!",
-                        },
-                    ]}
-                >
-                    <Input type="number" />
-                </Form.Item>
-
-                <Form.Item
-                    style={{ width: "600px" }}
-                    name="department"
-                    label="Department"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please select Department",
-                        },
-                    ]}
-                >
-                    <Select
-                        options={optionsDepartment}
-                        placeholder="Select Department"
-                    ></Select>
-                </Form.Item>
-                <Form.Item
-                    style={{ width: "600px" }}
-                    name="semester"
-                    label="Semester"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please select Semester",
-                        },
-                    ]}
-                >
-                    <Select
-                        options={optionsSemester}
-                        placeholder="Select Semester"
-                    ></Select>
-                </Form.Item>
-                <Form.Item
-                    style={{ width: "600px" }}
-                    name="session"
-                    label="Session"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Select session",
-                        },
-                    ]}
-                >
-                    <Select
-                        options={optionsSection}
-                        placeholder="Select session"
-                    ></Select>
-                </Form.Item>
-
-                <Form.List name="subject">
-                    {(fields, { add, remove }) => (
-                        <>
-                            {fields.map(({ key, name, ...restField }) => (
-                                <Space
-                                    key={key}
-                                    style={{
-                                        display: "flex",
-                                        marginBottom: 8,
-                                    }}
-                                    align="baseline"
-                                >
-                                    <Form.Item
-                                        {...restField}
-                                        name={[name, "subjectname"]}
-                                        rules={[
-                                            {
-                                                required: true,
-                                                message: "Missing first name",
-                                            },
-                                        ]}
-                                    >
-                                        <Input placeholder="Subject Name" />
-                                    </Form.Item>
-                                    <Form.Item
-                                        {...restField}
-                                        name={[name, "subjectcode"]}
-                                        rules={[
-                                            {
-                                                required: true,
-                                                message: "Missing last name",
-                                            },
-                                        ]}
-                                    >
-                                        <Input
-                                            placeholder="Subject Code "
-                                            type="number"
-                                        />
-                                    </Form.Item>
-                                    <Form.Item
-                                        {...restField}
-                                        name={[name, "score"]}
-                                        rules={[
-                                            {
-                                                required: true,
-                                                message: "Missing last name",
-                                            },
-                                        ]}
-                                    >
-                                        <Input
-                                            placeholder="Score"
-                                            type="number"
-                                        />
-                                    </Form.Item>
-                                    <Form.Item
-                                        {...restField}
-                                        name={[name, "gpa"]}
-                                        rules={[
-                                            {
-                                                required: true,
-                                                message: "Missing last name",
-                                            },
-                                        ]}
-                                    >
-                                        <Input placeholder="GPA " />
-                                    </Form.Item>
-                                    <MinusCircleOutlined
-                                        onClick={() => remove(name)}
-                                    />
-                                </Space>
-                            ))}
-                            <Form.Item>
-                                <Button
-                                    type="dashed"
-                                    onClick={() => add()}
-                                    block
-                                    icon={<PlusOutlined />}
-                                >
-                                    Add Subject
-                                </Button>
-                            </Form.Item>
-                        </>
-                    )}
-                </Form.List>
-
-                <Form.Item
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
-                >
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
-        </>
-    );
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
 };
 
 export default Home;
--- dashboard/src/pages/Login.jsx
+++ dashboard/src/pages/Login.jsx
@@ -3,88 +3,87 @@ import axios from "axios";
 import { Button, Form, Input } from "antd";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.svg";
+import login from "../assets/login.webp";
 
 const Login = () => {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const onFinish = async (values) => {
-        let data = {
-            email: values.email,
-            password: values.password,
-        };
-
-        let user_data = await axios.post("http://localhost:8000/login", data);
-        console.log(user_data);
-        if (user_data.data.success) {
-            navigate("/home");
-        }
+  const onFinish = async (values) => {
+    let data = {
+      email: values.email,
+      password: values.password,
     };
 
-    return (
-        <>
-            <div style={{ textAlign: "center", marginTop: "20px" }}>
-                <img src={logo} style={{ width: "100px" }} />
-            </div>
-            <Form
-                name="basic"
-                labelCol={{
-                    span: 4,
-                }}
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginTop: "20px",
-                }}
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                autoComplete="off"
-            >
-                <h1>Login Admin</h1>
-                <Form.Item
-                    style={{ width: "600px" }}
-                    label="Email"
-                    name="email"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your Email!",
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
+    let user_data = await axios.post("http://localhost:8000/login", data);
+    console.log(user_data);
+    if (user_data.data.success) {
+      navigate("/home");
+    }
+  };
 
-                <Form.Item
-                    style={{ width: "600px" }}
-                    label="Password"
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your password!",
-                        },
-                    ]}
-                >
-                    <Input.Password />
-                </Form.Item>
+  return (
+    <>
+      <div className="login_d">
+        <div className="login_img">
+          <img src={login} width={600} />
+        </div>
 
-                <Form.Item
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
-                >
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
-        </>
-    );
+        <Form
+          name="basic"
+          labelCol={{
+            span: 4,
+          }}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <div style={{ textAlign: "center", marginBottom: "20px" }}>
+            <img src={logo} style={{ width: "120px" }} />
+          </div>
+          <h1 style={{ marginBottom: "20px" }}>Login Admin</h1>
+          <Form.Item
+            style={{ width: "600px" }}
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Email!",
+              },
+            ]}
+          >
+            <Input placeholder="Enter your Email" />
+          </Form.Item>
+
+          <Form.Item
+            style={{ width: "600px" }}
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password placeholder="Enter your password" />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
+  );
 };
 
 export default Login;
